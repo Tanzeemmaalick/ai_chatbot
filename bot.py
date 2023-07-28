bc2d62ba44f8141b404e1016373204a588fbd934
Tanzeem Mallick <125208616+Tanzeemmaalick@users.noreply.github.com>
2023-07-28 16:30:48 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,21 @@
+from tkinter import*
+# your main code here
+
+root =Tk()
+
+
+
+root.geometry("400x500")
+root.iconbitmap("C:\\Users\\tanze\\OneDrive\\Desktop\\AI chatbot\\logo.ico")
+root.title("zeno")
+
+img = PhotoImage(file='C:\\Users\\tanze\\OneDrive\\Desktop\AI chatbot\\cb1.png')
+label = Label(image= img)
+label.pack()
+
+
+
+
+
+
+root.mainloop()
